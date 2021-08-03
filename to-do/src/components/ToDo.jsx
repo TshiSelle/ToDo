5667b69957d06f17beb289120b4ecf82974b8c05
OmarHoussami <omar.houssami20@gmail.com>
2021-08-03 23:07:26 +0300

    added css files

---
+++ to-do/src/components/ToDo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ToDo.css';
+
+function ToDo() {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar_header'>
+        <div className='sidebar_headerRight'></div>
+      </div>
+      <div className='sidebar-search'>
+        <div className='sidebar_searchContainer'></div>
+      </div>
+    </div>
+  );
+}
+
+export default ToDo;
