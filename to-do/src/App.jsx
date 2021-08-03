9ece41e597c2226bb35922d73cb8c591469c78fe
OmarHoussami <omar.houssami20@gmail.com>
2021-08-03 22:54:34 +0300

    added check boxes and text placeholders

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <input type='checkbox' />
+      <p>Text here</p>
+
+      <input type='checkbox' />
+      <p>Text here</p>
+
+      <input type='checkbox' />
+      <p>Text here</p>
+
+      <input type='checkbox' />
+      <p>Text here</p>
+    </div>
+  );
 }
 
 export default App;
