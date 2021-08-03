70123aea3de221eb0ce766a0ef419b6698b7a3a3
OmarHoussami <omar.houssami20@gmail.com>
2021-08-03 23:02:46 +0300

    background color

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -3,18 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <input type='checkbox' />
-      <p>Text here</p>
-
-      <input type='checkbox' />
-      <p>Text here</p>
-
-      <input type='checkbox' />
-      <p>Text here</p>
-
-      <input type='checkbox' />
-      <p>Text here</p>
+    <div className='app'>
+      <div className='app-body'></div>
     </div>
   );
 }
