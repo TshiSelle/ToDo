872a59d775000f7a92dd9fc47e99a3cc97ba69eb
OmarHoussami <omar.houssami20@gmail.com>
2021-08-03 23:44:15 +0300

    added done and to do sections

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import ToDo from './components/ToDo';
+import Done from './components/Done';
 
 function App() {
   return (
     <div className='app'>
       <div className='app_body'>
         <ToDo />
+        <Done />
       </div>
     </div>
   );
---
+++ to-do/src/components/Done.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Done() {
+  return (
+    <div className='done'>
+      <h1> Done</h1>
+    </div>
+  );
+}
+
+export default Done;
